57dd2ddc27e2317a5bb2ae97016303aae54a1a98
dbfl4524 <sinyuri0525@gmail.com>
2024-03-19 12:42:18 +0900

    chapter1

---
+++ screens/MainScreen.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MainScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text></Text>
+    </SafeAreaView>
+  );
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({});
