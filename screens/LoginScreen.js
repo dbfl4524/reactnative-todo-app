fe7df3e6430d797b892336ce00dd3c715c1703a6
dbfl4524 <sinyuri0525@gmail.com>
2024-03-24 06:12:58 +0900

    로그인/로그아웃 기능

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,15 +6,31 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ListIcon from "../assets/list.svg";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 import Toast from "react-native-toast-message";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigation = useNavigation();
   const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log("onAuthStateChanged user", user);
+      if (user) {
+        navigation.replace("Main");
+      }
+    });
+  }, []);
 
   const handleSignUp = async () => {
     try {
@@ -36,7 +52,17 @@ const LoginScreen = () => {
     }
   };
 
-  const handleLogin = async () => {};
+  const handleLogin = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      console.log(user);
+    } catch (error) {
+      Alert.alert("로그인 도중에 문제가 발생했습니다."),
+        error.message,
+        [{ text: "닫기", onPress: () => console.log("닫기") }],
+        { cancelable: true };
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -98,7 +124,7 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: "black",
-    width1: "100%",
+    width: "100%",
     padding: 15,
     borderRadius: 10,
     alignItems: "center",
